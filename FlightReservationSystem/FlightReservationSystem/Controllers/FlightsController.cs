3e7e84fcbf595180000f9a4ddafc020bf37a11a6
Ivan Grcic <100207993+TheGrca@users.noreply.github.com>
2024-06-20 22:56:26 +0200

    Added html pages

---
+++ FlightReservationSystem/FlightReservationSystem/Controllers/FlightsController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FlightReservationSystem.Models;
+
+namespace FlightReservationSystem.Controllers
+{
+    public class FlightsController : ApiController
+    {
+        private static List<Flight> flights = new List<Flight>();
+
+
+        // GET: api/Flights
+        [HttpGet]
+        public IEnumerable<Flight> GetFlights()
+        {
+            return flights;
+        }
+
+        // GET: api/Flights/{id}
+        [HttpGet]
+        public IHttpActionResult GetFlight(int id)
+        {
+            var flight = flights.SingleOrDefault(f => f.Id == id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            return Ok(flight);
+        }
+
+        // POST: api/Flights
+        [HttpPost]
+        public IHttpActionResult PostFlight(Flight flight)
+        {
+            flight.Id = flights.Count > 0 ? flights.Max(f => f.Id) + 1 : 1;
+            flight.FlightStatus = FlightStatus.Active;
+            flights.Add(flight);
+            return CreatedAtRoute("DefaultApi", new { id = flight.Id }, flight);
+        }
+
+        // PUT: api/Flights/{id}
+        [HttpPut]
+        public IHttpActionResult PutFlight(int id, Flight flight)
+        {
+            var existingFlight = flights.SingleOrDefault(f => f.Id == id);
+            if (existingFlight == null)
+            {
+                return NotFound();
+            }
+            if (existingFlight.FlightStatus == FlightStatus.Active || existingFlight.FlightStatus == FlightStatus.Active)
+            {
+                return BadRequest("Cannot change details of a flight with reservations.");
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/Flights/{id}
+        [HttpDelete]
+        public IHttpActionResult DeleteFlight(int id)
+        {
+            var flight = flights.SingleOrDefault(f => f.Id == id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            flights.Remove(flight);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
---
+++ FlightReservationSystem/FlightReservationSystem/Controllers/UsersController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FlightReservationSystem.Models;
+
+namespace FlightReservationSystem.Controllers
+{
+
+
+    public class UsersController : ApiController
+    {
+
+        private static List<User> users = new List<User>
+    {
+        new User
+        {
+            Username = "user",
+            Password = "user",
+            Name = "User",
+            Lastname = "User",
+            Email = "user@example.com",
+            DateOfBirth = new DateTime(2000, 1, 1),
+            Gender = Gender.Male,
+            TypeOfUser = TypeOfUser.Traveler,
+            Reservations = new List<Reservation>()
+        }
+    };
+
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            return users;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetUser(string username)
+        {
+            var user = users.SingleOrDefault(u => u.Username == username);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public IHttpActionResult PostUser(User user)
+        {
+            if (users.Any(u => u.Username == user.Username))
+            {
+                return BadRequest("Username already exists.");
+            }
+            users.Add(user);
+            return CreatedAtRoute("DefaultApi", new { id = user.Username }, user);
+        }
+
+        // PUT: api/Users/{username}
+        [HttpPut]
+        public IHttpActionResult PutUser(string username, User user)
+        {
+            var existingUser = users.SingleOrDefault(u => u.Username == username);
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+            existingUser.Password = user.Password;
+            existingUser.Name = user.Name;
+            existingUser.Lastname = user.Lastname;
+            existingUser.Email = user.Email;
+            existingUser.DateOfBirth = user.DateOfBirth;
+            existingUser.Gender = user.Gender;
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/Users/{username}
+        [HttpDelete]
+        public IHttpActionResult DeleteUser(string username)
+        {
+            var user = users.SingleOrDefault(u => u.Username == username);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            users.Remove(user);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
